9d0d3941c12f4c3368dd72f03d56d13354c49cc8
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2024-10-17 17:56:52 +0300

    Update Статистика_национальной_платежной_системы_Y.py

--- Банк_России/Статистика_национальной_платежной_системы_Y.py
+++ Банк_России/Статистика_национальной_платежной_системы_Y.py
@@ -8,7 +8,7 @@ from datetime import timedelta
 from tqdm import tqdm
 from tqdm.notebook import trange
 
-class Create_Class(object):
+class T1(object):
     def __init__(self, url):
         megasub = '/Макроэкономика и банки/Банк России/Статистика национальной платежной системы (Y)'
         sub = '/1. Институциональная обеспеченность платежными услугами'
@@ -17,8 +17,8 @@ class Create_Class(object):
         names = pd.Series(os.listdir('.'))
         for i in trange(len(names), leave = False):
             a = names[i]
-            date = a.replace('.xls', '')
-            b = pd.read_excel(a)
+            date = a[a.find("_")+1:a.find(".")]
+            b = pd.read_excel(a, skiprows=6, skipfooter=1, usecols= lambda x: x not in [1])
             b['Дата'] = date
             ### b['Дата'] = pd.to_datetime(b['Дата'])
             if i == 0: 
