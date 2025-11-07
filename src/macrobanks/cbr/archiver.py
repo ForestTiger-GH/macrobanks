8f8defe1dd07f1d0ec2165f7edd2f1558a98d06c
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2025-11-07 14:35:52 +0300

    Update archiver.py

--- src/macrobanks/cbr/archiver.py
+++ src/macrobanks/cbr/archiver.py
@@ -35,6 +35,7 @@ DEFAULT_URLS = [
     "https://www.cbr.ru/vfs/statistics/BankSector/Loans_to_corporations/01_11_F_Debt_sme_by_activity.xlsx",
     "https://www.cbr.ru/vfs/statistics/BankSector/Loans_to_corporations/01_13_F_Debt_sme_subj.xlsx",
     "https://www.cbr.ru/vfs/statistics/BankSector/Loans_to_corporations/01_13_I_Debt_sme_subj.xlsx",
+    "https://www.cbr.ru/vfs/statistics/BankSector/Loans_to_corporations/01_05_D_Debt_subj.xlsx",
     # Долговые бумаги
     "https://www.cbr.ru/vfs/statistics/debt_securities/66-debt_securities.xlsx", 
     # Средства совокупные
