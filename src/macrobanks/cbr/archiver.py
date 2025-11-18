be3a759fcb1e45ee3f2bb7eea134b93ec5e57eee
Dmitry <80126225+ForestTiger-GH@users.noreply.github.com>
2025-11-18 14:37:34 +0300

    Update archiver.py

--- src/macrobanks/cbr/archiver.py
+++ src/macrobanks/cbr/archiver.py
@@ -22,6 +22,9 @@ from urllib3.util.retry import Retry
 DEFAULT_URLS = [
     # Кредиты физлиц
     "https://www.cbr.ru/vfs/statistics/BankSector/Mortgage/02_05_Debt_ind.xlsx",
+    "https://www.cbr.ru/vfs/statistics/BankSector/Mortgage/02_14_Debt_mortgage.xlsx",
+    "https://www.cbr.ru/vfs/statistics/BankSector/Mortgage/02_18_Debt_scpa_mortgage.xlsx",
+    
     # Ипотека (полные ряды)
     "https://www.cbr.ru/vfs/statistics/banksector/mortgage/02_41_Mortgage_ihc.xlsx",
     "https://www.cbr.ru/vfs/statistics/BankSector/Mortgage/02_02_Mortgage.xlsx",
